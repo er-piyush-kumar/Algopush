6352ecd2a80ac43ef1c77cda523d0079c69b2a05
er-piyush-kumar <erpiyushkumar@outlook.com>
2025-04-28 00:44:01 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('./components/header.html')
+  fetch('../components/header.html')
     .then(res => res.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
     });
 
-  fetch('./components/footer.html')
+  fetch('../components/footer.html')
     .then(res => res.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
